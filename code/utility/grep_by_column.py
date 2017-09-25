e38e280c992cb4e25a5141ac4bda7647a2c94406
Yanyu Liang <yanyul@uchicago.edu>
2017-09-25 13:17:39 -0500

    added utility function to grep file by column

--- code/utility/grep_by_column.py
+++ code/utility/grep_by_column.py
@@ -1,1 +1,16 @@
+import argparse
+parser = argparse.ArgumentParser(prog='grep_by_column.py', description='''
+    This script takes an input file, column index to grep and the string
+    to grep
+''')
+parser.add_argument('file')
+parser.add_argument('icol', type = int)
+parser.add_argument('string')
+args = parser.parse_args()
+
 import os
+
+cmd = '''zcat {file} | \
+awk -F"\\t" '{{if (${icol} == "{string}") print $0;}}' \
+'''.format(file = args.file, icol = args.icol, string = args.string)
+os.system(cmd)
