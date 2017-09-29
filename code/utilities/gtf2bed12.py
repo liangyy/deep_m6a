fb1b5e15733142bcf025bb0786fa08257fbe86d8
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-09-29 14:05:22 -0500

    debugged gtf2bed

--- code/utilities/gtf2bed12.py
+++ code/utilities/gtf2bed12.py
@@ -2,23 +2,24 @@ import argparse
 parser = argparse.ArgumentParser(prog='gtf2bed12.py', description='''
     This script takes a GTF format file and outputs its corresponding
     BED12 format file.
-''')
-parser.add_argument('--read_method', default = 'zcat', help = '''
-    Specify the method to read input file (default is `zcat`)
+    WARNING: the name should end with gtf.gz
 ''')
 parser.add_argument('GTF')
 args = parser.parse_args()
 
 import os
+import re
 
-cmd = 'gtfToGenePred {gtf} {gtf}.genePred'.format(gtf = args.GTF)
+name = re.sub('.gtf.gz', '', args.GTF)
+
+cmd = 'gtfToGenePred {name}.gtf.gz {name}.genePred'.format(name = name)
 os.system(cmd)
 
-cmd = 'genePredToBed {gtf}.genePred {gtf}.bed12'.format(gtf = args.GTF)
+cmd = 'genePredToBed {name}.genePred {name}.bed12'.format(name = name)
 os.system(cmd)
 
-cmd = 'sort -k1,1 -k2,2n {gtf}.bed12 | gzip > {gtf}.sorted.bed12.gz'
+cmd = 'sort -k1,1 -k2,2n {name}.bed12 | gzip > {name}.sorted.bed12.gz'.format(name = name)
 os.system(cmd)
 
-cmd = 'rm {gtf}.genePred {gtf}.bed12'
+cmd = 'rm {name}.genePred {name}.bed12'.format(name = name)
 os.system(cmd)
